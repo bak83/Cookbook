d0f62e0feee28a31d42336d9dc50fa15b4bd7df0
bakda <bak.dariusz@gmail.com>
2022-11-13 15:25:48 +0100

    Add project files.

---
+++ Cookbook/Entities/Ingredients.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cookbook.Entities
+{
+	public class Ingredients
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		public string? NameOfIngredient { get; set; }
+	}
+}
---
+++ Cookbook/Entities/KindOfDiet.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cookbook.Entities
+{
+	public class KindOfDiet
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		public string? KindOfDish { get; set; }
+	}
+}
